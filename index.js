db720e8658865021cbdc44b5194240a0591e671d
akukamil <chamilioner_kid@mail.ru>
2022-11-04 21:40:27 +0300

    123

--- index.js
+++ index.js
@@ -3022,7 +3022,7 @@ req_dialog = {
 		//отправляем информацию о согласии играть с идентификатором игры
 		game_id=~~(Math.random()*999);
 		const fp = irnd(0,1);
-		const map_id = irnd(0,11);
+		const map_id = irnd(0,12);
 		const w_dev = rnd2(-0.05,0.05);
 		
 		
